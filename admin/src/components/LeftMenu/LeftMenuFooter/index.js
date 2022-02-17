c3180320a4bc64c853239963b575df1c1ad624f9
Pedro Miguel <pedro.miguel@pixter.com.br>
2022-02-17 15:36:56 -0300

    initial commit

---
+++ admin/src/components/LeftMenu/LeftMenuFooter/index.js
@@ -0,0 +1,31 @@
+/**
+ *
+ * LeftMenuFooter
+ *
+ */
+
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import Wrapper, { A } from './Wrapper';
+
+function LeftMenuFooter({ version }) {
+  // PROJECT_TYPE is an env variable defined in the webpack config
+  // eslint-disable-next-line no-undef
+  const projectType = PROJECT_TYPE;
+
+  return (
+    <Wrapper>
+      <div className="poweredBy">
+        <A key="website" href="https://strapi.io" target="_blank" rel="noopener noreferrer">
+          React Avançado
+        </A>
+      </div>
+    </Wrapper>
+  );
+}
+
+LeftMenuFooter.propTypes = {
+  version: PropTypes.string.isRequired,
+};
+
+export default LeftMenuFooter;
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd00485a9-65c5-427e-aa9a-c58178c21746'
+};
